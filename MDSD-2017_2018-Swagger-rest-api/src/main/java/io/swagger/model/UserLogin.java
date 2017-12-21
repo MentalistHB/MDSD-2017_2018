39bf833c9f1491878f69e343dc1cd242a4d5c6d7
MentalistTHB <nganyana@th-brandenburg.de>
2017-12-21 16:00:23 +0100

    Encryption added by UserLogin

--- MDSD-2017_2018-Swagger-rest-api/src/main/java/io/swagger/model/UserLogin.java
+++ MDSD-2017_2018-Swagger-rest-api/src/main/java/io/swagger/model/UserLogin.java
@@ -3,94 +3,100 @@ package io.swagger.model;
 import java.util.Objects;
 import io.swagger.annotations.ApiModelProperty;
 
-
-
-
 /**
  * UserLogin
  */
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2017-12-11T17:49:32.673+01:00")
 
-public class UserLogin   {
-  private String username = null;
+public class UserLogin {
+	private String username = null;
 
-  private String password = null;
+	private String password = null;
 
-  public UserLogin username(String username) {
-    this.username = username;
-    return this;
-  }
+	private boolean encryption;
 
-   /**
-   * Get username
-   * @return username
-  **/
-  @ApiModelProperty(required = true, value = "")
-  public String getUsername() {
-    return username;
-  }
+	public UserLogin username(String username) {
+		this.username = username;
+		return this;
+	}
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
+	/**
+	 * Get username
+	 * 
+	 * @return username
+	 **/
+	@ApiModelProperty(required = true, value = "")
+	public String getUsername() {
+		return username;
+	}
 
-  public UserLogin password(String password) {
-    this.password = password;
-    return this;
-  }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-   /**
-   * Get password
-   * @return password
-  **/
-  @ApiModelProperty(required = true, value = "")
-  public String getPassword() {
-    return password;
-  }
+	public UserLogin password(String password) {
+		this.password = password;
+		return this;
+	}
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+	/**
+	 * Get password
+	 * 
+	 * @return password
+	 **/
+	@ApiModelProperty(required = true, value = "")
+	public String getPassword() {
+		return password;
+	}
 
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    UserLogin userLogin = (UserLogin) o;
-    return Objects.equals(this.username, userLogin.username) &&
-        Objects.equals(this.password, userLogin.password);
-  }
+	@Override
+	public boolean equals(java.lang.Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		UserLogin userLogin = (UserLogin) o;
+		return Objects.equals(this.username, userLogin.username) && Objects.equals(this.password, userLogin.password);
+	}
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(username, password);
-  }
+	@Override
+	public int hashCode() {
+		return Objects.hash(username, password);
+	}
 
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class UserLogin {\n");
-    
-    sb.append("    username: ").append(toIndentedString(username)).append("\n");
-    sb.append("    password: ").append(toIndentedString(password)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("class UserLogin {\n");
 
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
+		sb.append("    username: ").append(toIndentedString(username)).append("\n");
+		sb.append("    password: ").append(toIndentedString(password)).append("\n");
+		sb.append("}");
+		return sb.toString();
+	}
+
+	public boolean isEncryption() {
+		return encryption;
+	}
+
+	public void setEncryption(boolean encryption) {
+		this.encryption = encryption;
+	}
+
+	/**
+	 * Convert the given object to string with each line indented by 4 spaces
+	 * (except the first line).
+	 */
+	private String toIndentedString(java.lang.Object o) {
+		if (o == null) {
+			return "null";
+		}
+		return o.toString().replace("\n", "\n    ");
+	}
 }
-
